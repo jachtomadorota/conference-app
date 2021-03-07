d581ffd1a67d4850ff6494538892111f11abcc53
jachtomadorota <jachtoma.do@gmail.com>
2021-03-07 10:10:36 +0100

    First commit

---
+++ src/test/java/com/dorotajachtoma/conferenceapp/ConferenceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.conferenceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
