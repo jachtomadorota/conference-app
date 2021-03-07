84e3cfacda29793c252a79783bc4b6d1fd95f752
jachtomadorota <jachtoma.do@gmail.com>
2021-03-07 17:14:00 +0100

    modify sessioncontroller

--- src/main/java/com/dorotajachtoma/conferenceapp/controller/SessionController.java
+++ src/main/java/com/dorotajachtoma/conferenceapp/controller/SessionController.java
@@ -1,8 +1,10 @@
 package com.dorotajachtoma.conferenceapp.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/api/v1/session")
 public class SessionController {
 }
