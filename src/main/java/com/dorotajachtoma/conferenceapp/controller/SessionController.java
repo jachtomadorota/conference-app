5d67378e45ec7bcc636b0f968084ba4c94060c0b
jachtomadorota <jachtoma.do@gmail.com>
2021-03-07 17:25:31 +0100

    create rest controller for session entitiy

--- src/main/java/com/dorotajachtoma/conferenceapp/controller/SessionController.java
+++ src/main/java/com/dorotajachtoma/conferenceapp/controller/SessionController.java
@@ -1,10 +1,34 @@
 package com.dorotajachtoma.conferenceapp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dorotajachtoma.conferenceapp.model.Session;
+import com.dorotajachtoma.conferenceapp.repository.SessionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/session")
 public class SessionController {
+
+    private final SessionRepository sessionRepository;
+
+    public SessionController(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Session> getAllSessions(){
+        return sessionRepository.findAll();
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET)
+    public Session getOneSession(@PathVariable Long id){
+        return sessionRepository.getOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Session createSession(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
 }
