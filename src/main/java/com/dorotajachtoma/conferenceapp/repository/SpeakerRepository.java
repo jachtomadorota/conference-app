4e2466d5bcc350ff86003613fdcaddceb679ecb2
jachtomadorota <jachtoma.do@gmail.com>
2021-03-07 15:24:29 +0100

    Add session and speaker repositories

---
+++ src/main/java/com/dorotajachtoma/conferenceapp/repository/SpeakerRepository.java
@@ -0,0 +1,10 @@
+package com.dorotajachtoma.conferenceapp.repository;
+
+
+import com.dorotajachtoma.conferenceapp.model.Speaker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpeakerRepository extends JpaRepository<Speaker,Long> {
+}
